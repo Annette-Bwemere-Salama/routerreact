db70458828945c3cb2d945b6a584b5f1b9c7f8df
annette Bwemere <bwemereannette@gmail.com>
2022-08-24 08:25:18 +0200

    refactory: suppression d'accolade sur le deuxieme ligne dans index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Expenses from "./routes/expenses";
@@ -15,5 +15,4 @@ root.render(
       </Route>
     </Routes>
   </BrowserRouter>
-
 );

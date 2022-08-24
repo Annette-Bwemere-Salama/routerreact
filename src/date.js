1ec8a3b12574a5339ce6d478dc2b4a2c1e8a4b03
annette Bwemere <bwemereannette@gmail.com>
2022-08-24 09:47:43 +0200

    feat: refactory dans data.js

--- src/date.js
+++ src/date.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Invoices from "./routes/invoices";
-
+import Invoice from "./invoice";
 
 export function getInvoices() {
-    return invoices;
-  }
-  
-  export function getInvoice(number) {
-    return invoices.find(
-      (invoice) => invoice.number === number
-    );
-  }
+  return invoices;
+}
+
+export function getInvoice(number) {
+  return invoices.find((invoice) => invoice.number === number);
+}
--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,31 +1,37 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { getInvoices } from "../data";
 
 
 
 export default function Invoices() {
-    let invoices = getInvoices();
+  let invoices = getInvoices();
   return (
 
-    <div style={{display: "flex"}}>
-    <nav
+    <div style={{ display: "flex" }}>
+      <nav
         style={{
-            borderRigth: "solid 1px",
-            padding: "1rem",
+          borderRigth: "solid 1px",
+          padding: "1rem",
         }}
-    >
-    {invoices.map((invoices)=>(
-        <Link
-        style={{display: "block", margin: "1rem 0"}}
-        to={`/invoices/${invoices.number}`}
-        key={invoices.number}
-        >
-        {invoices.name}
-        </Link>
-    ))}
-    </nav>
-    <Outlet />
+      >
+        {invoices.map((invoice) => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : "",
+              };
+            }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet />
     </div>
   );
 }
